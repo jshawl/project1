dcc6630281e6557a3defab1d2ae5be606d38a232
John Master <john.s.master@gmail.com>
2015-07-13 13:39:12 -0400

    Finally going to leave stringCard alone

--- source.js
+++ source.js
@@ -9,24 +9,65 @@ for (var i = 0; i < 52; i++){
   deck[j] = i;
 }
 
-function deal(){
-  var card = deck.pop();
+function deal(card){
+  var card = (card >= 0 ? card : deck.pop());
   var value = cardValue(card);
   hand.push(card);
   handVals.push(value);
   drawCard(card);
+  checkEnd();
 }
+function checkEnd(){
+  var points = handVals.reduce(function(a, b){return a + b;});
+  console.log(points);
+  if (points > 21){
+    var aceAt = handVals.indexOf(11);
+    if (aceAt >= 0){
+      handVals[aceAt] = 1;
+      return checkEnd();
+    }
+    console.log("Bust!");
+  }
+  else if (points == 21) {
+    if (checkBlackJack()){console.log("Black Jack!");}
+    else console.log("21!");
+  }
+}
+
+function checkBlackJack(){
+  return (hand.length == 2);
+}
+
 function drawCard(card){
   $(".hand").append($("<div>" + cardString(card) + "</div>")
                       .addClass("card")
              );
 }
 
+//Get point value of card -- could use tidying
 function cardValue(card){
   var val = Math.floor(card / 4) + 1;
   if (val == 1){val = 11;}
-  if (val > 10){val = 10;}
+  else if (val > 10){val = 10;}
   return val;
+}
+function stringCard(string){    //doesn't catch a card value out of range
+  var num = string.slice(0, string.length - 1).toUpperCase();
+  console.log(num);
+  var suit = string.slice(string.length - 1).toUpperCase();
+  console.log(suit);
+  var suits = ["H", "D", "C", "S"];
+  if (isNaN(num)){
+    if (num == "A"){num = "1";}
+    else if (num == "J"){num = "11"}
+    else if (num == "Q"){num = "12"}
+    else if (num == "K"){num = "13"}
+    else {num = -1;}
+    }
+  var suitNum = suits.indexOf(suit);
+  var card = (parseInt(num) - 1) * 4 + suitNum;
+  if (num < 0 || suitNum < 0){card = -1;}
+  return card;
 }
 
 function cardString(card){
